bdcb3a321b1ef2777c494bfb1bf9c61f25856fc9
Carlos Jesús Durbán Viloca <durban.vicar22@triana.salesianos.edu>
2021-10-26 09:56:53 +0200

    Añadida una línea

--- HolaMundo/src/holamundo/Principal.java
+++ HolaMundo/src/holamundo/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HolaMundo");
+		System.out.println("Vaya mierda de mundo");
 
 	}
 
